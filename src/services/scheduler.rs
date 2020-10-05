6fe2149903425ecb4f9771bfb08a62b448c2f64b
Tamme Schichler <tamme@schichler.dev>
2020-10-05 21:04:35 +0200

    cargo +nightly fmt

--- src/services/scheduler.rs
+++ src/services/scheduler.rs
@@ -1,26 +1,27 @@
-use {core::future::Future, rhizome_crate::extractable};
+use core::future::Future;
+use rhizome_crate::extractable;
 
 extractable! {
-    pub abstract trait Scheduler
+	pub abstract trait Scheduler
 
-    fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()> >);
+	fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()> >);
 }
 
 //TODO: This should be possible as `final` function in the extractable itself.
 impl dyn Scheduler {
-    pub fn spawn(&self, future: impl Future<Output = ()> + 'static) {
-        self.spawn_dynamic(Box::new(future))
-    }
+	pub fn spawn(&self, future: impl Future<Output = ()> + 'static) {
+		self.spawn_dynamic(Box::new(future))
+	}
 }
 
 impl Scheduler for fn(Box<dyn Future<Output = ()>>) {
-    fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
-        self(future)
-    }
+	fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
+		self(future)
+	}
 }
 
 impl Scheduler for Box<dyn Fn(Box<dyn Future<Output = ()>>)> {
-    fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
-        self(future)
-    }
+	fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
+		self(future)
+	}
 }
