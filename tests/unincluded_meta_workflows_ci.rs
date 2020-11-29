b55fbee9b8e292d4b08af35a9fee603c56fccb1a
Tamme Schichler <tamme@schichler.dev>
2020-11-29 23:58:23 +0100

    Test Rust version in CI workflow instead of .travis.yml

---
+++ tests/unincluded_meta_workflows_ci.rs
@@ -0,0 +1,10 @@
+#![cfg(not(miri))]
+
+#[path = "meta_constants_.rs"]
+mod constants;
+use constants::*;
+
+#[test]
+fn rust_version() {
+	version_sync::assert_contains_regex!(".github/workflows/ci.yml", &format!(r"^        rust: \[{},", RUST_VERSION));
+}
