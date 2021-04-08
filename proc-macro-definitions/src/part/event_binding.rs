fee4ce2b4258b5eeda31a6fe343b9f9727d64845
Tamme Schichler <tamme@schichler.dev>
2021-04-09 00:35:14 +0200

    Added FIXME regarding event binding soundness

--- proc-macro-definitions/src/part/event_binding.rs
+++ proc-macro-definitions/src/part/event_binding.rs
@@ -19,6 +19,9 @@ use syn_mid::Block;
 use tap::Pipe as _;
 use unquote::unquote;
 
+//FIXME: This is currently unsound, as `CallbackRegistration`s must be destroyed before any state (user-)accessible through the receiver reference.
+// As such, components will need a generated `Drop` implementation and drop them ahead of time..
+
 pub mod kw {
 	use syn::custom_keyword;
 	custom_keyword!(on);
