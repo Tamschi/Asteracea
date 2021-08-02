8883350e6e38c181a67825a193141ca016f0bd17
Tamme Schichler <tamme@schichler.dev>
2021-08-02 13:10:26 +0200

    Fixed remaining warnings

--- src/services/scheduler.rs
+++ src/services/scheduler.rs
@@ -10,18 +10,18 @@ extractable! {
 //TODO: This should be possible as `final` function in the extractable itself.
 impl dyn Scheduler {
 	pub fn spawn(&self, future: impl Future<Output = ()> + 'static) {
-		self.spawn_dynamic(Box::new(future))
+		self.spawn_dynamic(Box::new(future));
 	}
 }
 
 impl Scheduler for fn(Box<dyn Future<Output = ()>>) {
 	fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
-		self(future)
+		self(future);
 	}
 }
 
 impl Scheduler for Box<dyn Fn(Box<dyn Future<Output = ()>>)> {
 	fn spawn_dynamic(&self, future: Box<dyn Future<Output = ()>>) {
-		self(future)
+		self(future);
 	}
 }
