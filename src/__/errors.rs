c8d7ed0b11ec377d6e5675e305b2bcb893eaf53d
Tamme Schichler <tamme@schichler.dev>
2021-09-07 23:45:55 +0200

    Error harder on event binding misuse
    This stops `--cap-lints=warn` from making the code compile. Thanks for the catch, @Nemo157 !

--- src/__/errors.rs
+++ src/__/errors.rs
@@ -2,6 +2,11 @@ use std::marker::PhantomData;
 
 use lignin_schema::{NoBubbles, YesBubbles, YesCancelable};
 
+mod private {
+	pub enum Invalid {}
+}
+use private::Invalid;
+
 pub struct CheckNoBubbles<T: ?Sized>(PhantomData<T>);
 impl<T: ?Sized> CheckNoBubbles<T>
 where
@@ -32,27 +37,27 @@ where
 pub trait ActiveNotValid {
 	#[deprecated = "Keyword `active` is not valid for this event; the event is not cancelable."]
 	#[inline(always)]
-	fn check() {}
+	fn check(_: Invalid) {}
 }
 impl<T> ActiveNotValid for T {}
 
 pub trait BubbleNotValid {
 	#[deprecated = "Keyword `bubble` is not valid for this event; the event does not bubble."]
 	#[inline(always)]
-	fn check() {}
+	fn check(_: Invalid) {}
 }
 impl<T> BubbleNotValid for T {}
 
 pub trait CaptureNotValid {
 	#[deprecated = "Keyword `capture` is not valid for this event; the event does not bubble."]
 	#[inline(always)]
-	fn check() {}
+	fn check(_: Invalid) {}
 }
 impl<T> CaptureNotValid for T {}
 
 pub trait PhaseExpected {
 	#[deprecated = "Exected one of keywords `bubble` or `capture`; this event bubbles."]
 	#[inline(always)]
-	fn check() {}
+	fn check(_: Invalid) {}
 }
 impl<T> PhaseExpected for T {}
