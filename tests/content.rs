7502e1d71021ee8c9172169f0cf56df95432abce
Tamme Schichler <tamme@schichler.dev>
2021-12-24 05:20:39 +0100

    Added test for content transclusion output

--- tests/content.rs
+++ tests/content.rs
@@ -1,3 +1,7 @@
+use bumpalo::Bump;
+use ergo_pin::ergo_pin;
+use rhizome::Node;
+
 asteracea::component! {
 	Container()(..)
 
@@ -13,9 +17,27 @@ asteracea::component! {
 }
 
 asteracea::component! {
-	Parent()()
+	Parent()() -> Sync
 
 	<*Container
 		<*Content>
 	>
+}
+
+#[test]
+#[ergo_pin]
+fn content_in_container() {
+	let root = Node::new_for::<()>().into_arc();
+	let parent = pin!(Parent::new(&root, Parent::new_args_builder().build()).unwrap());
+	let bump = Bump::new();
+	let vdom = parent
+		.as_ref()
+		.render(&bump, Parent::render_args_builder().build())
+		.unwrap();
+	let mut html = String::new();
+	lignin_html::render_fragment(&vdom, &mut html, 3).unwrap();
+	assert_eq!(
+		html,
+		"<custom-container><custom-content></custom-content></custom-container>"
+	)
 }
