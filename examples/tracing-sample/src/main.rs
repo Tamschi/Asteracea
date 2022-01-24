18f6a48d2824ad441f9d7670e060ebb73dc284a2
Tamme Schichler <tamme@schichler.dev>
2022-01-24 17:15:03 +0100

    Example: Adjusted tracing-sample to be simpler and more informative

--- examples/tracing-sample/src/main.rs
+++ examples/tracing-sample/src/main.rs
@@ -1,29 +1,37 @@
-use asteracea::bumpalo::Bump;
+use asteracea::{bumpalo::Bump, __::AnonymousContentParentParameters};
 use core::pin::Pin;
 use rhizome::Node;
 use tracing::instrument;
 use tracing_flame::FlameLayer;
 use tracing_subscriber::prelude::*;
 
+#[derive(Debug)]
+struct A;
+#[derive(Debug)]
+struct B;
+#[derive(Debug)]
+struct C;
+#[derive(Debug)]
+struct D;
+#[derive(Debug)]
+struct E;
+
 asteracea::component! {
-	FirstAndMaybeSecond()(
-		recurse?: usize,
+	#[allow(clippy::needless_question_mark)] //FIXME
+	ContainerComponent(
+		// Constructor parameters:
+		_plain: A,
+		priv _stored: B,
+		_optional?: C,
+	)(
+		// Render parameters:
+		_plain: D,
+		_optional?: u64 = 0,
+		destructured @ E { .. }: E,
+		..,
 	) -> Sync
 
-	<ul
-		//FIXME: Replace `{}` with `()` and make it optional.
-		<li !"recurse = {:?}"(recurse)>
-
-		spread if {let Some(recurse) = recurse} <li
-			spread if {recurse > 1} [
-				"Nested with "<code ".recurse">":" <br>
-				defer box <*FirstAndMaybeSecond .recurse={recurse - 1}>
-			] else [
-				"Nested without "<code ".recurse">":" <br>
-				defer box <*FirstAndMaybeSecond>
-			]
-		>
-	/ul>
+	..
 }
 
 fn main() {
@@ -34,7 +42,7 @@ fn main() {
 #[must_use]
 fn set_up_tracing() -> impl Drop {
 	let tree_layer = tracing_tree::HierarchicalLayer::default()
-		.with_bracketed_fields(true)
+		.with_bracketed_fields(false)
 		.with_wraparound(10); // Wraparound is helpful for deeply nested GUIs.
 	let (flame_layer, flush_guard) = FlameLayer::with_file("./tracing-sample.folded").unwrap();
 
@@ -48,28 +56,35 @@ fn set_up_tracing() -> impl Drop {
 
 #[ergo_pin::ergo_pin]
 fn render_components() {
-	let app = FirstAndMaybeSecond::new(
+	let app = ContainerComponent::new(
 		&Node::new_for::<()>().into_arc(),
-		FirstAndMaybeSecond::new_args_builder().build(),
+		ContainerComponent::new_args_builder()
+			._plain(A)
+			._stored(B)
+			.build(),
 	)
 	.unwrap();
 	let app = pin!(app).as_ref();
 
 	let mut bump = Bump::new();
 
-	for recursion in 0..5 {
-		print_app(&bump, app, recursion);
-		bump.reset();
-	}
+	print_app(&bump, app);
+	bump.reset();
 }
 
 #[instrument(skip(bump, app))]
-fn print_app(bump: &Bump, app: Pin<&FirstAndMaybeSecond>, recursion: usize) {
+fn print_app(bump: &Bump, app: Pin<&ContainerComponent>) {
 	let vdom = app
 		.render(
 			bump,
-			FirstAndMaybeSecond::render_args_builder()
-				.recurse(recursion)
+			ContainerComponent::render_args_builder()
+				._plain(D)
+				.destructured(E)
+				.__Asteracea__anonymous_content((
+					//FIXME: This should not be directly typed.
+					AnonymousContentParentParameters {},
+					Box::new(|_| Ok(asteracea::lignin::Node::Multi(&[]))),
+				))
 				.build(),
 		)
 		.unwrap();
