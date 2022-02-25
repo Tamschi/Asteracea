34297da31f1dc289c83001b6b6b438cdb2720679
Tamme Schichler <tamme@schichler.dev>
2022-02-25 02:26:12 +0100

    Added missing documentation

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -9,4 +9,8 @@ mod invalidator;
 pub use content_runtime::ContentRuntime;
 pub use invalidator::Invalidator;
 
+/// The type of (most) owned handles that are injected when using a trait as dependency key.
+///
+/// There is no technical requirement that a trait will specify this type as [`Extract::Extracted`](`rhizome::sync::Extract::Extracted`),
+/// but it is by far the most straightforward option for owned sharing handles.
 pub type ServiceHandle<Service> = RefExtracted<TypeId, <Service as RefExtract>::ExtractedTarget>;
