3fbb17da5507dd792fac54efbf54da1dd76896e5
Tamme Schichler <tamme@schichler.dev>
2022-02-27 22:00:52 +0100

    Added test for dependency injection by container componets towards their children

--- tests/dependency_injection_by_container.rs
+++ tests/dependency_injection_by_container.rs
@@ -1,10 +1,17 @@
+use asteracea::services::Invalidator;
 use bumpalo::Bump;
 use ergo_pin::ergo_pin;
+use rhizome::sync::Inject;
 use rhizome::sync::Node;
 use std::any::TypeId;
+use std::task::Context;
 
 asteracea::component! {
 	Container()(..)
+
+	new with {
+		<dyn Invalidator>::inject(local_resource_node.borrow(), |_: Option<&mut Context<'_>>| ());
+	}
 
 	<"custom-container"
 		..
@@ -12,7 +19,9 @@ asteracea::component! {
 }
 
 asteracea::component! {
-	Content()()
+	Content(
+		dyn _invalidator: dyn Invalidator,
+	)()
 
 	<"custom-content">
 }
